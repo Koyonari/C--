99735e6c5ccf244a56d6e0bb480926179b3016d5
An Yong Shyan <yongshyan.an@gmail.com>
2024-08-20 02:20:25 +0800

    feat & docs: dynamic memory

--- Readme.md
+++ Readme.md
@@ -1220,7 +1220,39 @@ int main() {
 /*Output:
 The value of x is: 123*/
 ```
+### Dynamic Memory - Makes programs more flexible, useful when memory needed is unknown
+```cpp
+// if new is used, delete should be used to free memory
+// new operator - allocate memory
+// delete operator - free memory
+#include <iostream>
 
+using std::cout;
+using std::cin;
+using std::string;
+
+int main() {
+    char *pGrades = NULL;
+    int size;
+
+    cout << "Enter number of grades: ";
+    cin >> size;
+
+    pGrades = new char[size];
+    for (int i = 0; i < size; i++){
+        cout << "Enter grade #" << i + 1 << ": ";
+        cin >> pGrades[i];
+    }
+
+    for (int i = 0; i < size; i++){
+        cout << "Grade #" << i + 1 << ": " << pGrades[i] << "\n";
+    }
+
+    delete[] pGrades;
+
+    return 0;
+}
+```
 
 
 ___
--- main_practice/learn-cpp.cpp
+++ main_practice/learn-cpp.cpp
@@ -1,23 +1,30 @@
-// nullptr = keyword representing a null pointer
-// when using pointers, check if the pointer is null before dereferencing it
-// deferencing null pointers can cause runtime errors
+// if new is used, delete should be used to free memory
+// new operator - allocate memory
+// delete operator - free memory
 #include <iostream>
+
 using std::cout;
 using std::cin;
 using std::string;
 
 int main() {
+    char *pGrades = NULL;
+    int size;
 
-    int *pointer = nullptr;
-    int x = 123;
-    pointer = &x; // pointer now holds the address of x
+    cout << "Enter number of grades: ";
+    cin >> size;
 
-    if (pointer != nullptr) {
-        cout << "The value of x is: " << *pointer << "\n";
+    pGrades = new char[size];
+    for (int i = 0; i < size; i++){
+        cout << "Enter grade #" << i + 1 << ": ";
+        cin >> pGrades[i];
     }
-    else {
-        cout << "Address is not assigned, you tried deferencing a null pointer\n";
+
+    for (int i = 0; i < size; i++){
+        cout << "Grade #" << i + 1 << ": " << pGrades[i] << "\n";
     }
+
+    delete[] pGrades;
 
     return 0;
 }

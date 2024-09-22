5f4840a2f8403edbfc7a7462fc07f07a962d7d5d
An Yong Shyan <yongshyan.an@gmail.com>
2024-09-23 04:40:12 +0800

    feat: linkedlist append, print, destructor

--- dsa/LinkedList/LinkedList.cpp
+++ dsa/LinkedList/LinkedList.cpp
@@ -3,56 +3,111 @@ using namespace std;
 
 class Node {
     public:
-    int data;
-    Node *next;
+        int value;
+        Node *next;
 
-    Node(int data) {
-        this->data = data;
-        next = NULL;
-    }
+        Node(int val) {
+            value = val;
+            next = nullptr;
+    } 
 };
 
 class LinkedList{
     private:
-    Node *header;
-    Node *tail;
-    int size;
+        Node *head;
+        Node *tail;
+        int length;
 
     public:
-    LinkedList() {
-        header = NULL;
-        tail = NULL;
-        size = 0;
-    }
-
-    int getSize() {
-        return size;
-    }
-
-    // LinkedList Append
-    void append(int data){
-        // Create new Node
-        Node *n = new Node(data);
-
-        // Case 1: Empty List
-        if (header == NULL){
-            header = n;
-            tail = n;
-        } 
-        // Case 2: Non-empty List
-        else {
-            tail->next = n;
-            tail = n;
+        // Constructor
+        LinkedList(int value) {
+            Node *newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
         }
 
-        // Increase size as a new node has been added
-        size++;
-    }
+        // Destructor
+        ~LinkedList() {
+            Node *temp = head;
+            while (head) {
+                head = head->next; // Head points to next node
+                delete temp; // Deletes node
+                temp = head; // Temp points to head
+                // Loop continues until head is nullptr
+            }
+            // Confirmation
+            cout << "Linked List Destroyed" << endl;
+        }
 
-    // LinkedList Prepend
-    
+        // Print List
+        void printList() {
+            Node *temp = head;
+            cout << "List: ";
+            while (temp) {
+                cout << temp->value << " ";
+                temp = temp->next;
+            };
+        }
+
+        void getHead() {
+            cout << "Head: " << head->value << endl;
+        }
+
+        void getTail() {
+            cout << "Tail: " << tail->value << endl;
+        }
+
+        void getLength() {
+            cout << "Length: " << length << endl;
+        }
+
+        int getSize() {
+            return length;
+        }
+
+        // Append
+        void append(int data){
+            // Create new Node
+            Node *n = new Node(data);
+
+            // Case 1: Empty List
+            if (length == 0){
+                head = n;
+                tail = n;
+            } 
+            // Case 2: Non-empty List
+            else {
+                tail->next = n;
+                tail = n;
+            }
+
+            // Increase size as a new node has been added
+            length++;
+        }    
+
+        // Pop
+        void pop() {
+            
+        }
 };
 
 int main() {
+    LinkedList *myLinkedList = new LinkedList(4);
+    // Print List
+    myLinkedList->printList();
+    cout << endl;
+    myLinkedList->getHead();
+    myLinkedList->getTail();
+    myLinkedList->getLength();
+    
+    // Append
+    myLinkedList->append(5);
+    myLinkedList->printList();
+    cout << endl;
+    myLinkedList->getHead();
+    myLinkedList->getTail();
+    myLinkedList->getLength();
 
+    delete myLinkedList;
 }
